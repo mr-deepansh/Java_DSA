731ded77f7c2cf6fab7d0986dfecb1218c0e11a0
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-05-23 09:25:21 +0530

    feat: Add AreaC class to calculate and print the area of a circle

---
+++ 01_Basic/AreaC.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class AreaC {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the radius of circle: ");
+    float radius = sc.nextFloat();
+
+    float area = 3.14f * radius * radius;
+    System.out.println("Area of circle is: " + area);
+  }
+
+}
