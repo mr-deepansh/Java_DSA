77f770e1d1d06d145d73f72e2c343ab17a1f30da
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-05-24 21:21:17 +0530

    Refactor Basic class to calculate and print the result of 2 * (a + b)

---
+++ 02_Operators/Basic.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Basic {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int result = 2 * (a + b);
+    System.out.println("The result is " + result);
+  }
+}
