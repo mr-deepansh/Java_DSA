ca8b77276d04a345bd035157fb2839fdde875381
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-05-28 21:24:34 +0530

    Refactor Large_no class to improve code readability and remove unnecessary comments

---
+++ 03_Statements/Large_no.java
@@ -0,0 +1,19 @@
+/**
+ * Large_no
+ */
+public class Large_no {
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 40;
+    int c = 20;
+    if (a > b && a > c) {
+      System.out.println("a is large");
+    } else if (b > a && b > c) {
+      System.out.println("b is large");
+    } else {
+      System.out.println("c is large");
+    }
+
+  }
+}
