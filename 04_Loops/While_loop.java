18f7fbfe3fa32f575d290db1affc020b95044e14
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-05-30 11:37:40 +0530

    Refactor While_loop class for improved code readability and remove unnecessary comments

--- 04_Loops/While_loop.java
+++ 04_Loops/While_loop.java
@@ -1,20 +1,29 @@
-/**
- * While_loop
- */
+import java.util.Scanner;
+
 public class While_loop {
 
   public static void main(String[] args) {
-    int i = 0;
-    while (i < 10) {
-      System.out.println("Hello Programmer ! How are you Today");
-      i++;
-    }
+    // int i = 0;
+    // while (i < 10) {
+    // System.out.println("Hello Programmer ! How are you Today");
+    // i++;
+    // }
 
     // while loop second example
-    int counter = 1;
-    while (counter <= 10) {
-      System.out.println("Counter is: " + counter);
-      counter++;
+    // int counter = 1;
+    // while (counter <= 10) {
+    // System.out.println("Counter is: " + counter);
+    // counter++;
+
+    // third example
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int number = 1;
+    int n = input.nextInt();
+    while (number <= n) {
+      System.out.println(number);
+      number++;
 
     }
   }
