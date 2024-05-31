b7af4bc9785e3eab06117ecb7b750082e9466125
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-05-31 22:45:14 +0530

    Refactor Ques class for improved code readability and remove unnecessary comments

---
+++ 04_Loops/Ques.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ques {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // do {
+    // System.out.print("Enter a number ");
+    // int n = sc.nextInt();
+    // if (n % 10 == 0) {
+    // break;
+    // }
+    // System.out.println(n);
+    // } while (true);
+
+    while (true) {
+      System.out.println("Enter a number: ");
+      if (sc.nextInt() % 10 == 0) {
+        break;
+      }
+    }
+  }
+}
+
+// import java.util.Scanner;
+
+// public class Ques {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// do {
+// System.out.print("Enter a number: ");
+// int n = sc.nextInt();
+// if (n > 20) {
+// break;
+// }
+// System.out.println(n);
+// } while (true);
+// }
+// }
