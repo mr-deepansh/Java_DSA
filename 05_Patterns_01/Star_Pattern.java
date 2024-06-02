77d6a78d2ef17772349d551298e63f1637f758e9
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-06-02 21:27:21 +0530

    Refactor Star_Pattern class for improved code readability and remove unnecessary comments

--- 05_Patterns_01/Star_Pattern.java
+++ 05_Patterns_01/Star_Pattern.java
@@ -5,17 +5,17 @@ public class Star_Pattern {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number of rows: ");
     int n = sc.nextInt();
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
-        System.out.print("* ");
-      }
-      System.out.println();
-    }
+    // for (int i = 0; i < n; i++) {
+    // for (int j = 0; j <= i; j++) {
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
 
     // ! reverse pattern printing
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j <= i; j++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= (n - i + 1); j++) {
         System.out.print("* ");
       }
       System.out.println();
