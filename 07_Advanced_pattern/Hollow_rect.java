7482c3bad4fa977ba36bbdb9e69b1543fe5358c5
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-06-13 14:08:28 +0530

    refactor: Remove commented code and improve code readability in Hollow_rect class

--- 07_Advanced_pattern/Hollow_rect.java
+++ 07_Advanced_pattern/Hollow_rect.java
@@ -1,6 +1,8 @@
 // Hollow Rectangle pattern
 
 public class Hollow_rect {
+
+  // * Hollow Rectangle pattern
   public static void pattern(int totRow, int totCol) {
     // ! outer loop for rows
     for (int i = 1; i <= totRow; i++) {
@@ -18,8 +20,24 @@ public class Hollow_rect {
 
   }
 
+  // * Half Pyramid pattern
+  public static void Half_Pyramid(int n) {
+    for (int i = 1; i <= n; i++) {
+      // spaces
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print("");
+      }
+      // stars
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
   public static void main(String[] args) {
-    pattern(7, 5);
+    // pattern(7, 5);
+    Half_Pyramid(5);
   }
 
 }
