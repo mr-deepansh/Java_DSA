9cb915c1d262974fbd4373d1b95c3c93d2ddf934
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-06-18 22:18:00 +0530

    feat: Add zero_one_triangle method to Hollow_rect class

--- 07_Advanced_pattern/Hollow_rect.java
+++ 07_Advanced_pattern/Hollow_rect.java
@@ -55,11 +55,25 @@ public class Hollow_rect {
     }
   }
 
+  public static void zero_one_triangle(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        if ((i + j) % 2 == 0) {
+          System.out.print("1 ");
+        } else {
+          System.out.print("0 ");
+        }
+      }
+      System.out.println();
+    }
+  }
+
   public static void main(String[] args) {
     // pattern(7, 5);
     // Half_Pyramid(5);
     // Number_Pyramid(8);
     // floyds_triangle(5);
+    zero_one_triangle(5);
   }
 
 }
