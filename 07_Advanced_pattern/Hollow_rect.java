39b7dfb2f4b184f8adc4dfe2834da174ded1b60b
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-06-20 23:37:16 +0530

    some pravtice pattern

--- 07_Advanced_pattern/Hollow_rect.java
+++ 07_Advanced_pattern/Hollow_rect.java
@@ -107,6 +107,44 @@ public class Hollow_rect {
 
   }
 
+  public static void solid_rhombus(int n) {
+    for (int i = 1; i <= n; i++) {
+      // spaces
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+      // stars
+      for (int j = 1; j <= n; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+
+  }
+
+  public static void Daimaond_pattern(int n) {
+    // 1st half
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+    // 2nd half
+    for (int i = n; i >= 1; i--) {
+      for (int j = 1; j <= n - i; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 1; j <= 2 * i - 1; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
   public static void main(String[] args) {
     // pattern(7, 5);
     // Half_Pyramid(5);
@@ -114,6 +152,8 @@ public class Hollow_rect {
     // floyds_triangle(5);
     // zero_one_triangle(5);
     // Butterfly_Pattern(4);
+    // solid_rhombus(10);
+    Daimaond_pattern(7);
   }
 
 }
