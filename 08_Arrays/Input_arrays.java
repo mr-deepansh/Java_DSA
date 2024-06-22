32bda978ea461b724eead70649ddbc18e00763a0
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-06-22 05:39:52 +0530

    feat: Add Input_arrays class for inputting and calculating subject marks

---
+++ 08_Arrays/Input_arrays.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Input_arrays {
+
+  public static void main(String[] args) {
+    int[] marks = new int[5];
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter your Math marks: ");
+    marks[0] = sc.nextInt();
+
+    System.out.print("Enter your Physics marks: ");
+    marks[1] = sc.nextInt();
+
+    System.out.print("Enter your Chemistry marks: ");
+    marks[2] = sc.nextInt();
+
+    System.out.println("Your Math Subject marks is " + marks[0]);
+    System.out.println("Your Physics Subject marks is " + marks[1]);
+    System.out.println("Your Chemistry Subject marks is " + marks[2]);
+    sc.close();
+
+    // percentage calculation
+    int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+    System.out.println("Your overall percentage is " + percentage + "%");
+  }
+}
