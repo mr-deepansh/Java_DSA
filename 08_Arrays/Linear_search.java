6bee80ce07418b6e7947ab575bf6587e1d9d9e8f
coder-deep1 <deepanshgangwar7037@gmail.com>
2024-06-22 13:33:24 +0530

    refactor: Update Linear_search class to accept user input for the key to be searched

--- 08_Arrays/Linear_search.java
+++ 08_Arrays/Linear_search.java
@@ -1,10 +1,15 @@
+
 /**
  * Linear_search
  */
+import java.util.Scanner;
+
 public class Linear_search {
   public static void main(String[] args) {
     int number[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int key = 7;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the key to be searched : ");
+    int key = sc.nextInt();
     int index = linearSearch(number, key);
     if (index == -1) {
       System.out.println("The key is not found");
