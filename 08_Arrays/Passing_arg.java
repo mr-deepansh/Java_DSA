661fb7258cbb0e2a950d4c3d4982cad4b6309b2b
mr-deepansh <deepanshgangwar7037@gmail.com>
2024-12-07 21:35:34 +0530

    code minor mistake changes

--- 08_Arrays/Passing_arg.java
+++ 08_Arrays/Passing_arg.java
@@ -5,16 +5,16 @@ public class Passing_arg {
 
   public static void main(String[] args) {
     int noneChangeable = 210;
-    int number[] = { 5, 8, 5, 7 };
+    int[] number = { 5, 8, 5, 7 };
     updateNumber(number, noneChangeable);
     System.out.println(noneChangeable);
     // print numbers
-    for (int i = 0; i < number.length; i++) {
-      System.out.println(number[i] + " ");
+    for (int j : number) {
+      System.out.println(j + " ");
     }
   }
 
-  public static void updateNumber(int number[], int noneChangeable) {
+  public static void updateNumber(int[] number, int noneChangeable) {
     noneChangeable = 19;
     for (int i = 0; i < number.length; i++) {
       number[i] = number[i] * 2;
